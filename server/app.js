67511d33b341f8479c9d9f53837c014a9a9fe30a
Hunter Harbin <hharbin@gannett.com>
2015-02-22 20:16:12 -0500

    adding placeholder 404 and 500 responses

--- server/app.js
+++ server/app.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var port = process.env.PORT || 5000;
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
-app.set('port', (process.env.PORT || 5000));
 app.use(express.static(path.normalize(__dirname + "/..") + '/client'));
 require('./routes')(app);
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+app.listen(port, function() {
+  console.log("server listening on port " + port);
 });
--- server/routes.js
+++ server/routes.js
@@ -1,12 +1,26 @@
 'use strict';
 
-var errors = require('./components/errors'),
-    path    = require('path');
+var path       = require('path'),
+    clientPath = path.normalize(__dirname + "/..") + '/client/';
 
 module.exports = function(app) {
     app.get('/', function(req, res) {
-        res.sendFile(path.normalize(__dirname + "/..") + '/client/index.html');
+        res.sendFile(clientPath + 'index.html');
     });
 
-    app.route('/*').get(errors[404]);
+    app.get('/test', function(req, res) {
+        res.send('test page');
+    });
+
+    // 404
+    app.use(function(req, res) {
+        res.status(404);
+        res.render(__dirname + '/views/404.html');
+    });
+
+    // 500
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.send('500!!' + { error: err});
+    });
 };
